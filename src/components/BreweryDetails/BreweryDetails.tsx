f7eba5f97f5bec63eaaa8b420db4dea279d5a982
zcarpen <zcarpen@gmail.com>
2022-07-30 19:49:10 -0500

    saves progress before map gets added

--- src/components/BreweryDetails/BreweryDetails.tsx
+++ src/components/BreweryDetails/BreweryDetails.tsx
@@ -1,5 +1,11 @@
 import {Link} from 'react-router-dom'
+import {Fragment} from 'react'
+import { FiExternalLink } from 'react-icons/fi'
+import { MdLocationPin } from 'react-icons/md'
+import { BsTelephone } from 'react-icons/bs'
+import {FcContacts} from 'react-icons/fc'
 import { formatAddress } from '../../helperFunctions'
+import './BreweryDetails.scss'
 
 function BreweryDetails({listOfBreweries, cityState}) {
   // let params = useParams()
@@ -10,19 +16,46 @@ function BreweryDetails({listOfBreweries, cityState}) {
   }
   const id = queryParams.get("id")
   const {brewery_type, latitude, longitude, name, phone, postal_code, state, street, website_url} = listOfBreweries.find(brewery => brewery.id === id)
-
+  const position = [latitude, longitude];
   const address = formatAddress(street, state, postal_code, cityState);
   console.log(address)
+
   return (
-    <div className="details-container">
-      <Link to="/">Back to breweries</Link>
-      <div>
-        <h1>FB</h1>
-        
+    <Fragment>
+      <div className="card">
+        <Link className="back" to="/">Back to breweries</Link>
+        <div className={`brewery-type ${brewery_type}`} >{brewery_type}</div>
+        <div className="general-info">
+          <div className="icon-container">
+            <FcContacts className="contact-icon"/>
+          </div>
+          <div className="contact-details">
+            <h1>{name}</h1>
+            {address && 
+              <a>
+                <MdLocationPin/>
+                <span>{address}</span>
+              </a>
+            }
+            {phone && 
+              <a>
+                <BsTelephone/>
+                <span>{phone}</span>
+              </a>
+            }
+            {
+            website_url && 
+              <a target="_blank" href={website_url}>
+                <FiExternalLink/>
+                <span>{website_url}</span>
+              </a>
+              }
+          </div>
+        </div>
+        <div className="map-container">
+        </div>
       </div>
-
-
-    </div>
+    </Fragment>
   )
 }
 
