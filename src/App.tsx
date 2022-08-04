ab4ac6960d421e1f840dcdcdb4a92147d17186c6
zcarpen <zcarpen@gmail.com>
2022-08-04 09:15:36 -0500

    production fix for BreweryDetails

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,43 @@ import { Brewery } from './types/brewery';
 import BreweriesList from './components/BreweriesList/BreweriesList';
 import BreweryDetails from './components/BreweryDetails/BreweryDetails';
 import Search from './components/Search/Search';
-import { formatNewBreweries, fetchBreweries } from './helperFunctions/helperFunctions';
-
+import { formatNewBreweries } from './helperFunctions/helperFunctions';
 
 function App() {
   const [listOfBreweries, setListOfBreweries] = useState<Brewery[]>([])
   const [isLoading, setIsLoading] = useState(true);
   const [search, setSearch] = useState<string[]>(['austin', 'texas'])
 
-  const breweriesFetcher = useCallback(async () => {
-    fetchBreweries(search, setListOfBreweries, setIsLoading)
-  }, [search])
+  // const fetchBreweries = useCallback(() => {
+  //   try {
+  //     const result = await axios.get(`https://api.openbrewerydb.org/breweries?by_city=${search[0]}&by_state=${search[1]}&per_page=50&sort=asc`);
+  //     const newBreweries = formatNewBreweries(result.data)
+      
+  //     setListOfBreweries(newBreweries)
+  //     setIsLoading(false)
+
+  //   } catch(err) {
+  //     setIsLoading(false) // if time permits, display an error
+
+  //   }
+  // }, [search])
 
   useEffect(() => {
-    breweriesFetcher()
+    const fetchBreweries = async() => {
+      try {
+        const result = await axios.get(`https://api.openbrewerydb.org/breweries?by_city=${search[0]}&by_state=${search[1]}&per_page=50&sort=asc`);
+        const newBreweries = formatNewBreweries(result.data)
+        
+        setListOfBreweries(newBreweries)
+        setIsLoading(false)
+
+      } catch(err) {
+        setIsLoading(false) // if time permits, display an error
+
+      }
+    }
+
+    fetchBreweries()
   }, [search])
 
   if (isLoading) return <div className="app loading">LOADING...</div> //keeps rest of app from loading and renders a message
